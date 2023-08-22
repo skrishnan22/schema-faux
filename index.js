bd027f6b7d024de6a3f0304096565c729085a7a1
skrishnan22 <skrish22195@gmail.com>
2023-08-22 23:20:59 +0530

    Base for faker options, with Enum support

--- index.js
+++ index.js
@@ -32,6 +32,10 @@ function _getMockValue(fieldName, fieldType, fakerOptions = {}) {
     fieldName.toLowerCase().includes(field)
   );
 
+  if (fakerOptions.enum && fakerOptions.enumValues) {
+    return faker.helpers.arrayElement(fakerOptions.enumValues);
+  }
+
   if (matchingCommonField) {
     fakerMethod = commonFieldsToFakerMap[matchingCommonField];
     return faker[fakerMethod.module][fakerMethod.type]();
@@ -43,7 +47,19 @@ function _getMockValue(fieldName, fieldType, fakerOptions = {}) {
 
   return null;
 }
-
+/**
+ * Use mongoose field options like enum, min, max etc to form options to use while
+ * creating faker object
+ * @param {object} mongooseField
+ */
+function _constructFakerOptions(mongooseField) {
+  const fakerOptions = {};
+  if (mongooseField?.enumValues?.length) {
+    fakerOptions.enum = true;
+    fakerOptions.enumValues = mongooseField.enumValues;
+  }
+  return fakerOptions;
+}
 /**
  * Create a mock object based on mongoose schema
  * @param {*} schema - mongoose schema
@@ -67,7 +83,8 @@ function generateMock(schema, options = {}) {
     else if (fieldType === "Array") {
       mock[fieldName] = _mockArrayDataType(field, fieldName);
     } else {
-      mock[fieldName] = _getMockValue(fieldName, fieldType);
+      const fakerOptions = _constructFakerOptions(field);
+      mock[fieldName] = _getMockValue(fieldName, fieldType, fakerOptions);
     }
   }
 
@@ -113,7 +130,10 @@ const userSchema = new mongoose.Schema({
     field2: {
       field4: Date,
       field6: {
-        field7: String,
+        field7: {
+            type: String,
+            enum: ['either this', 'or that']
+        },
         field9: field9Schema,
       },
     },
